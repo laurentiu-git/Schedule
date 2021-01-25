e65092410b1b69a89752a09806a439488fcd6183
Laurentiu <Laurentiu@DESKTOP-DU0RLR6>
2021-01-25 20:10:49 +0200

    Ktlint added

--- app/src/test/java/com/example/schedule/ExampleUnitTest.kt
+++ app/src/test/java/com/example/schedule/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
+
 package com.example.schedule
 
+import org.junit.Assert.* //ktlint-disable
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
