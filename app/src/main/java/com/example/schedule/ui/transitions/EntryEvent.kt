1830a9f85e7e1bb7f8b77927ef9969df3e9a759e
Laurentiu <Laurentiu@DESKTOP-DU0RLR6>
2021-01-27 21:29:26 +0200

    Added hide keyboard for the Add Layout

--- app/src/main/java/com/example/schedule/ui/transitions/EntryEvent.kt
+++ app/src/main/java/com/example/schedule/ui/transitions/EntryEvent.kt
@@ -1,12 +1,13 @@
 package com.example.schedule.ui.transitions
 
+import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.view.inputmethod.InputMethodManager
 import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.schedule.R
 
 class EntryEvent : FrameLayout {
@@ -32,14 +33,23 @@ class EntryEvent : FrameLayout {
         }
 
         val closeElement = findViewById<TextView>(R.id.close)
-        closeElement.setOnClickListener{
+        closeElement.setOnClickListener {
             entryEvent?.onCloseClicked()
         }
 
+        val addLayout = findViewById<ConstraintLayout>(R.id.addLayout)
+        addLayout.setOnClickListener {
+            hideKeyboardFrom(context, it)
+        }
     }
 
     fun setAddEventListener(entryEventListener: EntryEventListener) {
         this.entryEvent = entryEventListener
+    }
+
+    fun hideKeyboardFrom(context: Context, view: View) {
+        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
 
