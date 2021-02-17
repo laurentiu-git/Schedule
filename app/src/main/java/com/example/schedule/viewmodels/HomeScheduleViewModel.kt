fe28e1c05c662d08da13ed382973c2b5354107c5
Laur <70561735+laurentiu-git@users.noreply.github.com>
2021-02-17 14:57:33 +0200

    Removing unused functions

--- app/src/main/java/com/example/schedule/viewmodels/HomeScheduleViewModel.kt
+++ app/src/main/java/com/example/schedule/viewmodels/HomeScheduleViewModel.kt
@@ -11,39 +11,12 @@ import java.util.* //ktlint-disable
 class HomeScheduleViewModel @ViewModelInject constructor(
     private val scheduleItemsItemsRepository: ScheduleItemsRepository
 ) : ViewModel() {
-
-    val daySchedule: MutableLiveData<Resource<String>> = MutableLiveData()
-    val schedule = MediatorLiveData<List<ScheduleInfo>>()
-
+    
     val date: MutableLiveData<Date> = MutableLiveData()
     var someSchedule: LiveData<List<ScheduleInfo>> = MutableLiveData()
 
-    init {
-        getDay()
-    }
-
-    private fun getDay() = viewModelScope.launch {
-        daySchedule.postValue(Resource.Loading())
-        val response = scheduleItemsItemsRepository.getScheduleItems()
-        daySchedule.postValue(scheduleItemsResponse(response))
-    }
-
-    private fun scheduleItemsResponse(response: String): Resource<String>? {
-        if (response == "Yolo" || response == "30-01-2021") {
-            return Resource.Success(response)
-        }
-
-        return Resource.Error("some error")
-    }
-
     fun updateAndReplace(schedule: ScheduleInfo) = viewModelScope.launch {
         scheduleItemsItemsRepository.updateAndReplace(schedule)
-    }
-
-    fun schedule(date: Date) {
-        schedule.addSource(scheduleItemsItemsRepository.getSchedules(date)) {
-            schedule.postValue(it)
-        }
     }
 
     fun deleteSchedule(schedule: ScheduleInfo) = viewModelScope.launch {
@@ -51,17 +24,10 @@ class HomeScheduleViewModel @ViewModelInject constructor(
     }
 
     fun getDate(amount: Int): Date {
-        //  schedule(scheduleItemsItemsRepository.getDate(amount))
         val currentDate = scheduleItemsItemsRepository.getDate(amount)
         date.postValue(currentDate)
         return currentDate
     }
-
-    /*  fun getDate(amount: Int, swipe: Boolean): Date {
-          schedule(scheduleItemsItemsRepository.getDate(amount))
-          return getDate(0)
-      }
-     */
 
     fun currentScheduleList() {
         someSchedule = Transformations.switchMap(
