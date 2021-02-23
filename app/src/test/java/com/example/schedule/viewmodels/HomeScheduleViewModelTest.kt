dfcb154055a3b0cc23c1199e52f5e33dcae1b2f0
Laur <70561735+laurentiu-git@users.noreply.github.com>
2021-02-23 21:46:25 +0200

    Implemented tests

--- app/src/test/java/com/example/schedule/viewmodels/HomeScheduleViewModelTest.kt
+++ app/src/test/java/com/example/schedule/viewmodels/HomeScheduleViewModelTest.kt
@@ -1,12 +1,24 @@
 package com.example.schedule.viewmodels
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.schedule.MainCoroutineRule
 import com.example.schedule.data.models.ScheduleInfo
 import com.example.schedule.repository.FakeScheduleItemsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class HomeScheduleViewModelTest {
     private lateinit var viewModel: HomeScheduleViewModel
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
     @Before
     fun setup() {
@@ -16,5 +28,18 @@ class HomeScheduleViewModelTest {
     @Test
     fun `inserting item`() {
         viewModel.updateAndReplace(ScheduleInfo(null, viewModel.getDate(0), "", "", "", "", ""))
+    }
+
+    @Test
+    fun `delete item`() {
+        val scheduleInfo = ScheduleInfo(null, viewModel.getDate(0), "", "", "", "", "")
+        viewModel.updateAndReplace(scheduleInfo)
+        viewModel.deleteSchedule(scheduleInfo)
+    }
+
+    @Test
+    fun `get schedule list`() {
+        val scheduleInfo = ScheduleInfo(null, viewModel.getDate(0), "", "", "", "", "")
+        viewModel.updateAndReplace(scheduleInfo)
     }
 }
