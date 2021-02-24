346a17c28d8aa7d88445af7f25b337fb30bd46ca
Laur <70561735+laurentiu-git@users.noreply.github.com>
2021-02-24 16:12:12 +0200

    Setup fakeDB via hilt for testing

---
+++ app/src/androidTest/java/com/example/schedule/data/local/ScheduleDaoTest.kt
@@ -0,0 +1,57 @@
+package com.example.schedule.data.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.filters.SmallTest
+import com.example.schedule.data.models.ScheduleInfo
+import com.example.schedule.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Named
+
+@ExperimentalCoroutinesApi
+@SmallTest
+@HiltAndroidTest
+class ScheduleDaoTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    @Named("test_db")
+    lateinit var db: ScheduleDatabase
+    lateinit var dao: ScheduleDao
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        dao = db.getScheduleDao()
+    }
+
+    @After
+    fun tearDown() {
+        db.close()
+    }
+
+    @Test
+    fun insertScheduleItem() = runBlockingTest {
+        val date = Calendar.getInstance().time
+        val scheudle = ScheduleInfo(1, date, "", "", "", "", "")
+        dao.updateAndReplace(scheudle)
+
+        val allScheduleItems = dao.getSchedule(date).getOrAwaitValue()
+
+        assertThat(allScheduleItems).contains(scheudle)
+    }
+}
