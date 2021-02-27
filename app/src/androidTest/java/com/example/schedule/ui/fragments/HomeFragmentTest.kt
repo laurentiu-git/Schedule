102257ac3c014cd74ebfc109b9c8afa4daa8cd63
Laur <70561735+laurentiu-git@users.noreply.github.com>
2021-02-27 12:54:45 +0200

    Added fragment tests

--- app/src/androidTest/java/com/example/schedule/ui/fragments/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/example/schedule/ui/fragments/HomeFragmentTest.kt
@@ -1,13 +1,13 @@
 package com.example.schedule.ui.fragments
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.action.ViewActions.* // ktlint-disable
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.* // ktlint-disable
 import androidx.test.filters.MediumTest
 import com.example.schedule.R
 import com.example.schedule.util.TestMethods
+import com.example.schedule.util.TestMethods.Companion.clickXY
 import com.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -57,8 +57,18 @@ class HomeFragmentTest {
         launchFragmentInHiltContainer<HomeFragment> ()
         onView(withId(R.id.addEvent)).perform(click())
         Thread.sleep(2000)
-        onView(withId(R.id.entryEvent)).perform(TestMethods.clickXY(10,10))
+        onView(withId(R.id.entryEvent)).perform(clickXY(10, 10))
         Thread.sleep(1000)
         onView(withId(R.id.entryEvent)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
+    }
+
+    @Test
+    fun openKeyboardFromAddScheduleView_ExpectKeyboardToBeVisible() {
+        launchFragmentInHiltContainer<HomeFragment> ()
+        onView(withId(R.id.addEvent)).perform(click())
+        assert(!TestMethods.isKeyboardShown())
+        Thread.sleep(1000)
+        onView(withId(R.id.titleText)).perform(click())
+        assert(TestMethods.isKeyboardShown())
     }
 }
--- app/src/androidTest/java/com/example/schedule/util/TestMethods.kt
+++ app/src/androidTest/java/com/example/schedule/util/TestMethods.kt
@@ -1,25 +1,32 @@
 package com.example.schedule.util
 
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.GeneralClickAction
 import androidx.test.espresso.action.Press
 import androidx.test.espresso.action.Tap
-
+import androidx.test.platform.app.InstrumentationRegistry
 
 class TestMethods {
- companion object{
-     fun clickXY(x: Int, y: Int): ViewAction? {
-         return GeneralClickAction(
-             Tap.SINGLE,
-             { view ->
-                 val screenPos = IntArray(2)
-                 view.getLocationOnScreen(screenPos)
-                 val screenX = (screenPos[0] + x).toFloat()
-                 val screenY = (screenPos[1] + y).toFloat()
-                 floatArrayOf(screenX, screenY)
-             },
-             Press.FINGER
-         )
-     }
- }
+    companion object {
+        fun clickXY(x: Int, y: Int): ViewAction? {
+            return GeneralClickAction(
+                Tap.SINGLE,
+                { view ->
+                    val screenPos = IntArray(2)
+                    view.getLocationOnScreen(screenPos)
+                    val screenX = (screenPos[0] + x).toFloat()
+                    val screenY = (screenPos[1] + y).toFloat()
+                    floatArrayOf(screenX, screenY)
+                },
+                Press.FINGER
+            )
+        }
+
+        fun isKeyboardShown(): Boolean {
+            val inputMethodManager = InstrumentationRegistry.getInstrumentation().targetContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            return inputMethodManager.isAcceptingText
+        }
+    }
 }
